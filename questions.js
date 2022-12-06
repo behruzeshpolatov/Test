a7ffd694b353d4f370bb9aef8493d127410d274c
behruzeshpolatov <116355462+behruzeshpolatov@users.noreply.github.com>
2022-12-06 20:47:05 +0500

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+
+    {
+        numb: 1,
+        question: "36+33-56+13+53=?",
+        answer: "79",
+        options: [
+            "174",
+            "79",
+            "34",
+            "45"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Mental arifmetika Asosiy quroli",
+        answer: "Abacus",
+        options: [
+            "QALAM",
+            "Abacus",
+            "Ruchka",
+            "Hech nima"
+        ]
+    },
+    {
+        numb: 3,
+        question: "34+45-66?",
+        answer: "13",
+        options: [
+            "1",
+            "2",
+            "13",
+            "4"
+        ]
+    },
+    {
+        numb: 4,
+        question: "76-55+23-22+56=?",
+        answer: "78",
+        options: [
+            "71",
+            "78",
+            "33",
+            "32"
+        ]
+    },
+    {
+        numb: 5,
+        question: "12-10+35+11+51?",
+        answer: "99",
+        options: [
+            "99",
+            "98",
+            "97",
+            "96"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Endigi testlar 2 xonali soni 1 xonali songa ko'paytirishdan.Tayyormisiz?",
+        answer: "Ha",
+        options: [
+            "Ha",
+            "Yo'q",
+            
+        ]
+    },
+    {
+        numb: 7,
+        question: "34x5?",
+        answer: "170",
+        options: [
+            "150",
+            "160",
+            "97",
+            "170"
+        ]
+    },
+    {
+        numb: 8,
+        question: "24x2?",
+        answer: "48",
+        options: [
+            "150",
+            "48",
+            "97",
+            "56"
+        ]
+    },
+    {
+        numb: 9,
+        question: "16x5?",
+        answer: "80",
+        options: [
+            "10",
+            "60",
+            "80",
+            "70"
+        ]
+    },
+    {
+        numb: 10,
+        question: "18x3?",
+        answer: "54",
+        options: [
+            "24",
+            "34",
+            "44",
+            "54"
+        ]
+    },
+];
